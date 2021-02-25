25a8af6a48d4c202fd192a9b41c9ec9154461ab0
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2021-02-25 16:26:47 +0100

    Add all required components to the submit page

---
+++ src/components/SelectTranslationTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <select class="form-select" :id="id" @change="$emit('update:modelValue', parseInt($event.srcElement.value))" :value="modelValue">
+    <option
+      v-for="item in options"
+      :selected="item.value === def ? true : false"
+      :key="item.value"
+      :value="item.value"
+    >
+      {{ item.value }}: {{ item.label }}
+    </option>
+  </select>
+</template>
+<script>
+export default {
+  name: "SelectTranslationTable",
+  props: {
+    id: { type: String, default: "" },
+    def: { type: Number, default: 1 },
+    modelValue: { type: Number },
+  },
+  methods: {
+
+  },
+  emits: ['update:modelValue'],
+  data: function () {
+    return {
+      options: [
+        { value: "1", label: "The Standard Code" },
+        { value: "2", label: "The Vertebrate Mitochondrial Code" },
+        { value: "3", label: "The Yeast Mitochondrial Code" },
+        {
+          value: "4",
+          label:
+            "The Mold, Protozoan, and Coelenterate Mitochondrial Code and the Mycoplasma/Spiroplasma Code",
+        },
+        { value: "5", label: "The Invertebrate Mitochondrial Code" },
+        {
+          value: "6",
+          label: "The Ciliate, Dasycladacean and Hexamita Nuclear Code",
+        },
+        {
+          value: "9",
+          label: "The Echinoderm and Flatworm Mitochondrial Code",
+        },
+        { value: "10", label: "The Euplotid Nuclear Code" },
+        {
+          value: "11",
+          label: "The Bacterial, Archaeal and Plant Plastid Code",
+        },
+        { value: "12", label: "The Alternative Yeast Nuclear Code" },
+        { value: "13", label: "The Ascidian Mitochondrial Code" },
+        { value: "14", label: "The Alternative Flatworm Mitochondrial Code" },
+        { value: "16", label: "Chlorophycean Mitochondrial Code" },
+        { value: "21", label: "Trematode Mitochondrial Code" },
+        { value: "22", label: "Scenedesmus obliquus Mitochondrial Code" },
+        { value: "23", label: "Thraustochytrium Mitochondrial Code" },
+        { value: "24", label: "Rhabdopleuridae Mitochondrial Code" },
+        {
+          value: "25",
+          label: "Candidate Division SR1 and Gracilibacteria Code",
+        },
+        { value: "26", label: "Pachysolen tannophilus Nuclear Code" },
+        { value: "27", label: "Karyorelict Nuclear Code" },
+        { value: "28", label: "Condylostoma Nuclear Code" },
+        { value: "29", label: "Mesodinium Nuclear Code" },
+        { value: "30", label: "Peritrich Nuclear Code" },
+        { value: "31", label: "Blastocrithidia Nuclear Code" },
+        { value: "33", label: "Cephalodiscidae Mitochondrial UAA-Tyr Code" },
+      ],
+    };
+  },
+};
+</script>
--- src/pages/Submit.vue
+++ src/pages/Submit.vue
@@ -1,10 +1,249 @@
 <template>
   <page-header page="Submit" />
+  <div class="container page-body">
+    <div class="mb-3">
+      <textarea
+        class="form-control"
+        name="paste-fasta"
+        cols="120"
+        rows="15"
+        placeholder="Paste your fasta sequences here or select a fasta file from your computer below..."
+        v-model="sequence"
+      ></textarea>
+    </div>
+    <div class="input-group mb-3">
+      <input
+        class="form-control"
+        type="file"
+        id="fastaFile"
+        @change="fastaFileChanged($event.target.name, $event.target.files)"
+        accept=".fas,.fna,.fasta"
+      />
+    </div>
+    <hr />
+    <div class="mt-4">
+      <h4 class="mb-2">Organism</h4>
+      <div class="row">
+        <div class="col">
+          <input
+            class="form-control"
+            type="text"
+            id="genus"
+            placeholder="Genus"
+            v-model="genus"
+          />
+        </div>
+        <div class="col">
+          <input
+            class="form-control"
+            type="text"
+            id="species"
+            placeholder="Species"
+            v-model="species"
+          />
+        </div>
+        <div class="col">
+          <input
+            class="form-control"
+            type="text"
+            id="strain"
+            placeholder="Strain"
+            v-model="strain"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-4">
+      <h4 class="mb-2">Annotation</h4>
+      <div class="row">
+        <div class="col">
+          <div class="form-check mt-3">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              v-model="completeGenome"
+              id="complete-genome"
+            />
+            <label class="form-check-label" for="complete-genome">
+              Complete genome
+            </label>
+          </div>
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              v-model="keepContigHeaders"
+              id="keep-headers"
+            />
+            <label class="form-check-label" for="keep-headers">
+              Keep contig headers
+            </label>
+          </div>
+        </div>
+        <div class="col">
+          <label class="form-label" for="min-contig-length">
+            Min contig length
+          </label>
+          <input
+            class="form-control"
+            type="number"
+            v-model="minContigLength"
+            id="min-contig-length"
+          />
+        </div>
+        <div class="col">
+          <label class="form-label" for="translation-table">
+            Translation table
+          </label>
+          <select-translation-table
+            id="translation-table"
+            v-model="translationTable"
+          ></select-translation-table>
+        </div>
+        <div class="col">
+          <label class="form-label" for="mono-diderm">Mono-/Diderm</label>
+          <select-derm-type
+            id="mono-diderm"
+            v-model="dermType"
+          ></select-derm-type>
+        </div>
+        <div class="col">
+          <label class="form-label" for="prodigal-training-file">
+            Prodigal training file
+          </label>
+
+          <input
+            class="form-control"
+            type="file"
+            id="prodigal-training-file"
+            @change="
+              prodigalFileChanged($event.target.name, $event.target.files)
+            "
+            accept=".tf"
+          />
+        </div>
+      </div>
+      <div class="mt-4">
+        <h4 class="mb-2">Replicons</h4>
+        <div class="row">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Original sequence id</th>
+                <th>Length</th>
+                <th>New sequence id</th>
+                <th>Type</th>
+                <th>Topology</th>
+                <th>Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in fastaContent" :key="item.id">
+                <td>{{ item.id }}</td>
+                <td>{{ item.length }}</td>
+                <td><input type="text" v-model="item.new" /></td>
+                <td>{{ item.type }}</td>
+                <td>{{ item.topology }}</td>
+                <td>{{ item.name }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="d-flex justify-content-end mb-5">
+        <button
+          class="btn btn-secondary"
+          type="button"
+          id="submit-button"
+          @click="handle()"
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import PageHeader from "@/components/PageHeader";
+import SelectTranslationTable from "@/components/SelectTranslationTable";
+import SelectDermType from "../components/SelectDermType.vue";
 export default {
   name: "Submit",
-  components: { PageHeader },
+  components: { PageHeader, SelectTranslationTable, SelectDermType },
+
+  methods: {
+    fastaFileChanged: function (name, file) {
+      this.sequenceFile = file[0];
+    },
+    prodigalFileChanged: function (name, file) {
+      this.prodigalTrainingFile = file[0];
+    },
+    handle: function () {
+      console.log(this);
+    },
+  },
+  watch: {
+    translationTable(t) {
+      console.log(t);
+    },
+  },
+  data() {
+    return {
+      sequence: "",
+      sequenceFile: null,
+      translationTable: 11,
+      completeGenome: false,
+      keepContigHeaders: false,
+      minContigLength: 1,
+      dermType: "UNKNOWN",
+      prodigalTrainingFile: null,
+      genus: "",
+      species: "",
+      strain: "",
+      fastaContent: [
+        {
+          id: "NODE_1",
+          length: 123,
+          newid: "",
+          type: "",
+          topology: "",
+          name: "",
+        },
+        {
+          id: "NODE_2",
+          length: 123,
+          newid: "",
+          type: "",
+          topology: "",
+          name: "",
+        },
+        {
+          id: "NODE_3",
+          length: 123,
+          newid: "",
+          type: "",
+          topology: "",
+          name: "",
+        },
+        {
+          id: "NODE_4",
+          length: 123,
+          newid: "",
+          type: "",
+          topology: "",
+          name: "",
+        },
+      ],
+    };
+  },
 };
 </script>
+<style scoped>
+h4 {
+  color: gray;
+}
+form-label {
+  color: gray;
+}
+</style>
