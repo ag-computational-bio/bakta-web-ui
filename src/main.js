07f84568b1b6f0ec892f0d80e69d46081c329228
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2021-06-24 08:33:39 +0200

    Move api config to external file

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { expectJson } from "./fetch-helper";
+import { createApp } from "vue";
+import App from "@/App";
+import router from "@/router";
+import bakta from "@/bakta";
 
-import { createApp } from 'vue'
-import App from '@/App'
-import router from '@/router'
-import bakta from '@/bakta'
-
-const app = createApp(App)
+function init(config) {
+  const app = createApp(App)
     .use(router)
-    .use(bakta, {
-        api: "https://restapi.bakta.ingress.rancher2.computational.bio",
-        token: "d89vgaegfv0aew8cagfwecg"
-    })
-window.vm = app.mount('#app')
+    .use(bakta, config);
+  window.vm = app.mount("#app");
+}
+
+window
+  .fetch("config/config.json")
+  .then(expectJson)
+  .then((j) => init(j))
+  .catch(
+    () =>
+      (document.body.innerHTML =
+        'Could not load config file. Please check <a href="config/config.json">config</a>')
+  );
