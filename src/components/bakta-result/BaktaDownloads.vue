b62ddd5ba064c3664583b8ff0d5eb451b6fe8c1c
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2025-03-31 09:51:38 +0200

    Add file descriptions to download page

--- src/components/bakta-result/BaktaDownloads.vue
+++ src/components/bakta-result/BaktaDownloads.vue
@@ -1,10 +1,15 @@
 <template>
-  <ul class="list-group">
-    <li class="list-group-item" v-for="d of downloads" :key="d.label">
-      <a :href="d.url" class="me-2" download><i class="bi bi-download"></i></a>
-      <a :href="d.url" download> {{ d.label }}</a>
-    </li>
-  </ul>
+  <table class="table">
+    <tbody>
+      <tr v-for="d of downloads" :key="d.label">
+        <td>
+          <a :href="d.url" class="me-2" download><i class="bi bi-download"></i></a>
+          <a :href="d.url" download> {{ d.label }}</a>
+        </td>
+        <td>{{ d.description }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 <script setup lang="ts">
 import type { JobResult, ResultFileKey } from '@/model/job'
@@ -15,27 +20,70 @@ const props = defineProps<{
 }>()
 
 const downloads = computed(() => {
-  type Count = { label: string; position: number }
+  type Count = { label: string; position: number; description?: string }
   const order: Record<ResultFileKey, Count> = {
-    TSV: { label: 'tsv', position: 0 },
-    GFF3: { label: 'gff3', position: 10 },
-    GBFF: { label: 'gbff', position: 20 },
-    EMBL: { label: 'embl', position: 25 },
-    FFN: { label: 'ffn', position: 30 },
-    FAA: { label: 'faa', position: 35 },
-    FNA: { label: 'fna', position: 40 },
-    JSON: { label: 'json', position: 50 },
-    TSVHypothetical: { label: 'tsv (hypothetical)', position: 55 },
-    TSVInference: { label: 'tsv (inference)', position: 57 },
-    FAAHypothetical: { label: 'faa (hypothetical)', position: 60 },
-    PNGCircularPlot: { label: 'circular plot (png)', position: 70 },
-    SVGCircularPlot: { label: 'circular plot (svg)', position: 80 },
-    TXTLogs: { label: 'bakta logs', position: 90 },
+    TSV: {
+      label: 'tsv',
+      description: 'annotations as simple human readble TSV',
+      position: 0,
+    },
+    GFF3: { label: 'gff3', description: 'annotations & sequences in GFF3 format', position: 10 },
+    GBFF: {
+      label: 'gbff',
+      description: 'annotations & sequences in (multi) GenBank format',
+      position: 20,
+    },
+    EMBL: {
+      label: 'embl',
+      description: 'annotations & sequences in (multi) EMBL format',
+      position: 25,
+    },
+    FFN: { label: 'ffn', description: 'feature nucleotide sequences as FASTA', position: 30 },
+    FAA: { label: 'faa', description: 'CDS/sORF amino acid sequences as FASTA', position: 35 },
+    FNA: { label: 'fna', description: 'replicon/contig DNA sequences as FASTA', position: 40 },
+    JSON: {
+      label: 'json',
+      description: 'all (internal) annotation & sequence information as JSON',
+      position: 50,
+    },
+    TSVHypothetical: {
+      label: 'tsv (hypothetical)',
+      description:
+        'further information on hypothetical protein CDS as simple human readble tab separated values',
+      position: 55,
+    },
+    TSVInference: {
+      label: 'tsv (inference)',
+      description:
+        'inference metrics (score, evalue, coverage, identity) for annotated accessions as TSV',
+      position: 57,
+    },
+    FAAHypothetical: {
+      label: 'faa (hypothetical)',
+      description: 'hypothetical protein CDS amino acid sequences as FASTA',
+      position: 60,
+    },
+    PNGCircularPlot: {
+      label: 'circular plot (png)',
+      description: 'circular genome annotation plot as PNG',
+      position: 70,
+    },
+    SVGCircularPlot: {
+      label: 'circular plot (svg)',
+      description: 'circular genome annotation plot as SVG',
+      position: 80,
+    },
+    TXTLogs: {
+      label: 'bakta summary',
+      description: 'summary as TXT',
+      position: 90,
+    },
   }
   const resultFiles: Record<string, string> =
     props.job && props.job.ResultFiles ? props.job.ResultFiles : {}
 
-  const l: { key: string; label: string; position: number; url: string }[] = []
+  const l: { key: string; label: string; description?: string; position: number; url: string }[] =
+    []
   for (const k of Object.keys(resultFiles)) {
     if (k in order) {
       const _k = k as ResultFileKey
